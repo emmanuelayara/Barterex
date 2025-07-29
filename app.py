e5ce001c1a5eb252ad5adb7d6e40457452bf7d93
Emmanuel Ayara <ayaraemmanuel16@gmail.com>
2025-07-29 11:23:36 +0100

    commit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, redirect, url_for, request, flash, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
@@ -15,8 +16,11 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024  # Max size: 2MB
 
 app.config['SECRET_KEY'] = 'super-secret-key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///barter.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+# postgresql://barter_n7gd_user:txVmlNLLM5YFzI1bkCc28ixYSWTCfcKQ@dpg-d248b2ffte5s73ar0ba0-a.oregon-postgres.render.com/barter_n7gd
 
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
