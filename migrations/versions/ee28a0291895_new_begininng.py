0d269ab8a21678355e0bdc59f84a1a6bbc02f8b8
Emmanuel Ayara <ayaraemmanuel16@gmail.com>
2025-08-28 09:53:52 +0100

    changes to delivery page

--- migrations/versions/ee28a0291895_new_begininng.py
+++ migrations/versions/ee28a0291895_new_begininng.py
@@ -1,8 +1,8 @@
-"""migrate
+"""new begininng
 
-Revision ID: a5997555df2d
+Revision ID: ee28a0291895
 Revises: 
-Create Date: 2025-08-27 13:53:06.364369
+Create Date: 2025-08-28 09:40:52.460641
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'a5997555df2d'
+revision = 'ee28a0291895'
 down_revision = None
 branch_labels = None
 depends_on = None
