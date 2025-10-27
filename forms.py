c1e1e9bd413d049d1ef82cd92542d27e6c544b73
Emmanuel Ayara <ayaraemmanuel16@gmail.com>
2025-10-27 16:39:13 +0100

    made new changes to pickup stations

--- forms.py
+++ forms.py
@@ -33,9 +33,48 @@ class ProfileUpdateForm(FlaskForm):
     phone_number = StringField('Phone Number', validators=[Length(min=10, max=15)])
     address = TextAreaField('Address', validators=[Length(max=200)])
     city = StringField('City', validators=[Length(max=50)])
-    state = StringField('State', validators=[Length(max=50)])
+    state = SelectField('State', choices=[
+        ('', 'Select State'),  # Default empty option
+        ('Abia', 'Abia'),
+        ('Adamawa', 'Adamawa'),
+        ('Akwa Ibom', 'Akwa Ibom'),
+        ('Anambra', 'Anambra'),
+        ('Bauchi', 'Bauchi'),
+        ('Bayelsa', 'Bayelsa'),
+        ('Benue', 'Benue'),
+        ('Borno', 'Borno'),
+        ('Cross River', 'Cross River'),
+        ('Delta', 'Delta'),
+        ('Ebonyi', 'Ebonyi'),
+        ('Edo', 'Edo'),
+        ('Ekiti', 'Ekiti'),
+        ('Enugu', 'Enugu'),
+        ('FCT', 'Federal Capital Territory (Abuja)'),
+        ('Gombe', 'Gombe'),
+        ('Imo', 'Imo'),
+        ('Jigawa', 'Jigawa'),
+        ('Kaduna', 'Kaduna'),
+        ('Kano', 'Kano'),
+        ('Katsina', 'Katsina'),
+        ('Kebbi', 'Kebbi'),
+        ('Kogi', 'Kogi'),
+        ('Kwara', 'Kwara'),
+        ('Lagos', 'Lagos'),
+        ('Nasarawa', 'Nasarawa'),
+        ('Niger', 'Niger'),
+        ('Ogun', 'Ogun'),
+        ('Ondo', 'Ondo'),
+        ('Osun', 'Osun'),
+        ('Oyo', 'Oyo'),
+        ('Plateau', 'Plateau'),
+        ('Rivers', 'Rivers'),
+        ('Sokoto', 'Sokoto'),
+        ('Taraba', 'Taraba'),
+        ('Yobe', 'Yobe'),
+        ('Zamfara', 'Zamfara')
+    ], validators=[DataRequired()])
     profile_picture = FileField('Profile Picture', validators=[FileAllowed(['jpg', 'jpeg', 'png'], 'Images only!')])
-    submit = SubmitField ('Update Profile')
+    submit = SubmitField('Update Profile')
     
 
 
